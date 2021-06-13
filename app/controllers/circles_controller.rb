006988f13520b5f266bfd853e6d8948e3e2866cd
takuo-maeda <cafeacb@olivesheep34.sakura.ne.jp>
2021-06-13 09:29:12 +0900

    profileview完成

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -1,2 +1,7 @@
 class CirclesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  root 'circles#index'
+  resources :profiles
 end
