bb893a00a445cb1d679aaa2c2686bb45284c2b75
takuo-maeda <cafeacb@olivesheep34.sakura.ne.jp>
2021-06-14 23:25:31 +0900

    プロフィール登録完了

--- db/migrate/20210611115003_create_profiles.rb
+++ db/migrate/20210611115003_create_profiles.rb
@@ -5,16 +5,16 @@ class CreateProfiles < ActiveRecord::Migration[6.0]
       t.string        :first_name         ,null: false
       t.string        :family_name_prono  ,null: false
       t.string        :first_name_prono   ,null: false
-      t.string        :postal_code       ,null: false
-      t.string        :prefecture_id     ,null: false
-      t.string        :address           ,null: false
-      t.string        :street            ,null: false
+      t.string        :postal_code        ,null: false
+      t.string        :prefecture_id      ,null: false
+      t.string        :address            ,null: false
+      t.string        :street             ,null: false
       t.string        :building
-      t.string        :phone_number      ,null: false
-      t.date          :birthday          ,null: false
-      t.integer       :sex               ,null: false
-      t.integer       :body_weight       ,null: false
-      t.references    :user              ,null: false, foreign_key: true
+      t.string        :phone_number       ,null: false
+      t.date          :birthday           ,null: false
+      t.integer       :sex                ,null: false
+      t.integer       :body_weight        ,null: false
+      t.references    :user               ,null: false, foreign_key: true
       t.timestamps
     end
   end
