f78061e2b70349b803aca74f98dc61c224682347
takuo-maeda <cafeacb@olivesheep34.sakura.ne.jp>
2021-07-11 15:39:14 +0900

    JavaScript糖質計算式完成7月11日

--- app/javascript/packs/check.js
+++ app/javascript/packs/check.js
@@ -1,4 +1,16 @@
 
+window.addEventListener("load", () => {
+  const gCalorie = document.getElementById("calorie");
+  const gProtein = document.getElementById("protein");
+  const gLipid = document.getElementById("lipid");
+  gCalorie.addEventListener("input", () => {
+    const gSugarContent = document.getElementById("sugar-content");
+    gSugarContent.innerHTML = Math.round((gCalorie.value-gProtein.value-gLipid.value)/4/10);
+  });
+});
+
+
+
 window.addEventListener("change", () => {
   const checkbox = document.getElementById("check1");
 
