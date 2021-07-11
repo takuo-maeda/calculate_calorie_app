fd2dd1122d9269379fcbdda28f38edcb0dfc8366
takuo-maeda <cafeacb@olivesheep34.sakura.ne.jp>
2021-07-11 22:56:15 +0900

    調整中

--- app/javascript/packs/check.js
+++ app/javascript/packs/check.js
@@ -3,11 +3,32 @@ window.addEventListener("load", () => {
   const gCalorie = document.getElementById("calorie");
   const gProtein = document.getElementById("protein");
   const gLipid = document.getElementById("lipid");
+  const gFiber = document.getElementById("fiber");
+ 
   gCalorie.addEventListener("input", () => {
     const gSugarContent = document.getElementById("sugar-content");
-    gSugarContent.innerHTML = Math.round((gCalorie.value-gProtein.value-gLipid.value)/4/10);
+    gSugarContent.innerHTML = (Math.round((gCalorie.value-gProtein.value * 4-gLipid.value * 9)/4*10))/10-gFiber.value;
+    const gCarbo = document.getElementById("carbohydrate");
+    gCarbo.innerHTML = (Math.round((gCalorie.value-gProtein.value * 4 -gLipid.value * 9)/4*10))/10 + gFiber.value;
   });
-});
+  gProtein.addEventListener("input", () => {
+    const gSugarContent = document.getElementById("sugar-content");
+    gSugarContent.innerHTML = (Math.round((gCalorie.value-gProtein.value * 4 -gLipid.value * 9)/4*10))/10-gFiber.value;
+    const gCarbo = document.getElementById("carbohydrate");
+    gCarbo.innerHTML = (Math.round((gCalorie.value-gProtein.value * 4 -gLipid.value * 9)/4*10))/10 + gFiber.value;
+  });
+  gLipid.addEventListener("input", () => {
+    const gSugarContent = document.getElementById("sugar-content");
+    gSugarContent.innerHTML = (Math.round((gCalorie.value-gProtein.value * 4 -gLipid.value * 9)/4*10))/10-gFiber.value;
+    const gCarbo = document.getElementById("carbohydrate");
+    gCarbo.innerHTML = (Math.round((gCalorie.value-gProtein.value * 4 -gLipid.value * 9)/4*10))/10 + gFiber.value;
+  });
+  gFiber.addEventListener("input", () => {
+    const gSugarContent = document.getElementById("sugar-content");
+    gSugarContent.innerHTML = (Math.round((gCalorie.value-gProtein.value * 4 -gLipid.value * 9)/4*10))/10-gFiber.value;
+ 
+  });
+});M
 
 
 
